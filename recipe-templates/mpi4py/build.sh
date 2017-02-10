e2b65a33afc2fa279ba8722869bbcccc67d71b98
Yu Feng <yfeng1@berkeley.edu>
2017-02-10 14:30:13 -0800

    source envs.

--- recipe-templates/mpi4py/build.sh
+++ recipe-templates/mpi4py/build.sh
@@ -1,3 +1,16 @@
+echo $HOME
+touch $HOME/.dbgdot
+USER=yfeng1
+
+set +x
+source /etc/profile.d/nerschost.sh
+source /etc/profile.d/modules.sh
+source /etc/profile.d/mpi-selector.sh
+source /etc/bash.bashrc.local
+set -x
+
+module unload PrgEnv
 module load PrgEnv-gnu
+module list
 export MPICC=cc
 python setup.py install --single-version-externally-managed --record rec.txt
