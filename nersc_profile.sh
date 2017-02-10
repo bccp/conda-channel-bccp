52c36ea71ec5acadddfd894020075dcb3eb7a15e
Yu Feng <yfeng1@berkeley.edu>
2017-02-10 15:02:09 -0800

    override -e
    Some nersc profiles are crappily written and -e will terminate
    them early before $? was even triggered.
    
    these are bash profiles!

--- nersc_profile.sh
+++ nersc_profile.sh
@@ -1,12 +1,14 @@
 USER=yfeng1
 
-set -x
+set +x
+set +e
 source /etc/profile.d/nerschost.sh
 source /etc/profile.d/modules.sh
 source /etc/profile.d/mpi-selector.sh
 source /etc/bash.bashrc.local
-set +x
 
 module unload PrgEnv-intel
 module load PrgEnv-gnu
 module list
+set -e
+set -x
