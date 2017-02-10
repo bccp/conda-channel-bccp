7adf322443d92305a5ed649f98f9cebbe42cca28
Nick Hand <nhand@berkeley.edu>
2017-02-10 14:43:39 -0800

    source nersc profile

--- recipe-templates/mpi4py/build.sh
+++ recipe-templates/mpi4py/build.sh
@@ -1,16 +1,3 @@
-echo $HOME
-touch $HOME/.dbgdot
-USER=yfeng1
-
-set +x
-source /etc/profile.d/nerschost.sh
-source /etc/profile.d/modules.sh
-source /etc/profile.d/mpi-selector.sh
-source /etc/bash.bashrc.local
-set -x
-
-module unload PrgEnv
-module load PrgEnv-gnu
-module list
+source /usr/common/contrib/bccp/conda-channel-bccp/nersc_profile.sh
 export MPICC=cc
 python setup.py install --single-version-externally-managed --record rec.txt
