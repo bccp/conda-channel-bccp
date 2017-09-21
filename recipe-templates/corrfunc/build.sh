522f25042326402138aa37e5a3de3082ebbe4287
Yu Feng <rainwoodman@gmail.com>
2017-09-21 00:17:46 -0700

    pin glibc version of corrfunc.

---
+++ recipe-templates/corrfunc/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+if [[ $OSTYPE == darwin* ]]; then
+    export CFLAGS="-headerpad_max_install_names"
+    export CXXFLAGS=$CFLAGS
+else
+    export CFLAGS="-include $PWD/glibc-compat.h"
+    # corrfunc makefile purges CFLAGS; fix it here.
+    sed --in-place 's;CFLAGS:=;CFLAGS?=;' common.mk
+fi
+
+make install
+$PYTHON setup.py install --single-version-externally-managed --record rec.txt
+
+if [[ $OSTYPE != darwin* ]]; then
+    bash check-glibc.sh $SP_DIR/Corrfunc || exit 1
+fi
