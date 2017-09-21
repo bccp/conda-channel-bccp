14a68c0518c16de014a22d607b1a2677930c785d
Yu Feng <rainwoodman@gmail.com>
2017-09-21 12:15:54 -0700

    fitsio multiple file.

--- recipe-templates/fitsio/build.sh
+++ recipe-templates/fitsio/build.sh
@@ -7,6 +7,8 @@ else
     export CFLAGS="-include $PWD/glibc-compat.h"
 fi
 
+cp $RECIPE_DIR/../../glibc-compat.h .
+cp $RECIPE_DIR/../../check-glibc.sh .
 
 # add libm see https://github.com/esheldon/fitsio/pull/135
 patch < $RECIPE_DIR/patch.libm
