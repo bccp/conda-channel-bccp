cd9d83def31b1d6cb04f0a364bc02ee75f60aa39
Yu Feng <rainwoodman@gmail.com>
2017-09-23 16:08:21 -0700

    pin pmesh mpsort kdcount and pfft-python.

--- recipe-templates/pfft-python/build.sh
+++ recipe-templates/pfft-python/build.sh
@@ -2,12 +2,17 @@
 
 if [ `uname` == Darwin ]; then
 	export LIBS="-Wl,-rpath,$CONDA_PREFIX/lib"
+    export CXXFLAGS=$CFLAGS
+else
+    export CFLAGS="-include $PWD/glibc-compat.h"
+    export LIBS="-lm" # because the compat fails fftw3's -lm check
 fi
+
+cp $RECIPE_DIR/../../glibc-compat.h .
+cp $RECIPE_DIR/../../check-glibc.sh .
 
 $PYTHON setup.py install 
 
-# Add more build steps here, if they are necessary.
-
-# See
-# http://docs.continuum.io/conda/build.html
-# for a list of environment variables that are set during the build process.
+if [[ $OSTYPE != darwin* ]]; then
+    bash check-glibc.sh $SP_DIR/pfft || exit 1
+fi
