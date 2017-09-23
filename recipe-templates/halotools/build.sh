3465e42a2605b29f1590944ac2e6a884ee83ee05
Yu Feng <rainwoodman@gmail.com>
2017-09-23 16:58:45 -0700

    change halotools setup.py

--- recipe-templates/halotools/build.sh
+++ recipe-templates/halotools/build.sh
@@ -9,7 +9,7 @@ fi
 cp $RECIPE_DIR/../../glibc-compat.h .
 cp $RECIPE_DIR/../../check-glibc.sh .
 
-$PYTHON setup.py install --offline
+$PYTHON setup.py install --single-version-externally-managed --record rec.txt
 
 if [[ $OSTYPE != darwin* ]]; then
     bash check-glibc.sh $SP_DIR/$PKG_NAME || exit 1
