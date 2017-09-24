c459a2ace8fcf82098ad8df7a4c109770ab4a775
Yu Feng <rainwoodman@gmail.com>
2017-09-23 17:34:22 -0700

    add 'offline'.

--- recipe-templates/halotools/build.sh
+++ recipe-templates/halotools/build.sh
@@ -9,7 +9,7 @@ fi
 cp $RECIPE_DIR/../../glibc-compat.h .
 cp $RECIPE_DIR/../../check-glibc.sh .
 
-$PYTHON setup.py install --single-version-externally-managed --record rec.txt
+$PYTHON setup.py install --single-version-externally-managed --record rec.txt --offline
 
 if [[ $OSTYPE != darwin* ]]; then
     bash check-glibc.sh $SP_DIR/$PKG_NAME || exit 1
