d540a1a6307019b5630c2691fc7cca2abfff6d2a
Yu Feng <rainwoodman@gmail.com>
2017-11-17 01:10:06 -0800

    revert to system compiler on darwin.

--- recipe-templates/mpsort/build.sh
+++ recipe-templates/mpsort/build.sh
@@ -2,13 +2,6 @@
 
 # workaround mpich/openmpi not built with the new toolchain
 
-export OMPI_CPPFLAGS="$CPPFLAGS -I$CONDA_PREFIX/include"
-export OMPI_LDFLAGS="$LDFLAGS -L$CONDA_PREFIX/lib"
-export OMPI_LIBS=$LIBS
-export OMPI_CFLAGS="$CFLAGS -I$CONDA_PREFIX/include"
-export OMPI_CXXFLAGS=$CXXFLAGS
-export OMPI_FCFLAGS=$FCFLAGS
-
 if [[ $OSTYPE != darwin* ]]; then
     # for MAC use system compilers
     # because cross complilation doesn't work
@@ -18,6 +11,15 @@ if [[ $OSTYPE != darwin* ]]; then
     export MPICH_CXX=$CXX
     export OMPI_FC=$FC
     export MPICH_FC=$FC
+    export OMPI_CPPFLAGS="$CPPFLAGS -I$CONDA_PREFIX/include"
+    export OMPI_LDFLAGS="$LDFLAGS -L$CONDA_PREFIX/lib"
+    export OMPI_LIBS=$LIBS
+    export OMPI_CFLAGS="$CFLAGS -I$CONDA_PREFIX/include"
+    export OMPI_CXXFLAGS=$CXXFLAGS
+    export OMPI_FCFLAGS=$FCFLAGS
+else
+    export CFLAGS="-headerpad_max_install_names"
+    export CXXFLAGS=$CFLAGS
 fi
 
 echo --------------------
