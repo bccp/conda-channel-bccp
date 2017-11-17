a278d94a85b8a09b49b10662a850b9572482f033
Yu Feng <rainwoodman@gmail.com>
2017-11-17 01:20:56 -0800

    modify pfft-python to use native compiler on osx.

--- recipe-templates/pfft-python/build.sh
+++ recipe-templates/pfft-python/build.sh
@@ -2,20 +2,25 @@
 
 # workaround mpich/openmpi not built with the new toolchain
 
-export OMPI_CPPFLAGS="$CPPFLAGS -I$CONDA_PREFIX/include"
-export OMPI_LDFLAGS="$LDFLAGS -L$CONDA_PREFIX/lib"
-export OMPI_LIBS=$LIBS
-export OMPI_CC=$CC
-export MPICH_CC=$CC
-export OMPI_CFLAGS="$CFLAGS -I$CONDA_PREFIX/include"
-export OMPI_CXX=$CXX
-export MPICH_CXX=$CXX
-export OMPI_CXXFLAGS=$CXXFLAGS
-export OMPI_FC=$FC
-export MPICH_FC=$FC
-export OMPI_FCFLAGS=$FCFLAGS
-
-export MPICC=mpicc
+if [[ $OSTYPE != darwin* ]]; then
+    # for MAC use system compilers
+    # because cross complilation doesn't work
+    export OMPI_CC=$CC
+    export MPICH_CC=$CC
+    export OMPI_CXX=$CXX
+    export MPICH_CXX=$CXX
+    export OMPI_FC=$FC
+    export MPICH_FC=$FC
+    export OMPI_CPPFLAGS="$CPPFLAGS -I$CONDA_PREFIX/include"
+    export OMPI_LDFLAGS="$LDFLAGS -L$CONDA_PREFIX/lib"
+    export OMPI_LIBS=$LIBS
+    export OMPI_CFLAGS="$CFLAGS -I$CONDA_PREFIX/include"
+    export OMPI_CXXFLAGS=$CXXFLAGS
+    export OMPI_FCFLAGS=$FCFLAGS
+else
+    export CFLAGS="-headerpad_max_install_names"
+    export CXXFLAGS=$CFLAGS
+fi
 
 $PYTHON setup.py install 
 
