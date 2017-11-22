b443b7ab01a107e58c7f5e6776862f17b60dd2cb
Yu Feng <rainwoodman@gmail.com>
2017-11-21 22:26:47 -0800

    passing in extra parameters.

--- build-all.sh
+++ build-all.sh
@@ -1,18 +1,23 @@
+err=0
+#trap 'err=1' ERR;
+trap 'exit 1' ERR;
+
 VARIANT=$1
+shift
 
 pushd platform
 
-conda build mpich3
-conda build openmpi3
-conda build mpi4py
+conda build $* mpich3
+conda build $* openmpi3
+conda build $* mpi4py
+
+popd
 
 pushd recipes;
 
-err=0
-trap 'err=1' ERR;
 while read package ; do 
     echo Running ---- conda build -m ../$VARIANT $INSPECT $UPLOAD $package-*;
-    conda build -m ../$VARIANT $INSPECT $UPLOAD $package-*;
+    conda build -m ../$VARIANT $* $INSPECT $UPLOAD $package-*;
     echo Result ----- $err
 done < ../build-order;
 
