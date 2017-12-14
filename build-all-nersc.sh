670b4935bd1cacf05f24c22218edce3b86346ef2
bccpuser Pseudo User <bccpuser@cori17.nersc.gov>
2017-12-13 17:36:19 -0800

    no test no nersc (only computing nodes)

--- build-all-nersc.sh
+++ build-all-nersc.sh
@@ -4,7 +4,7 @@ trap 'exit 1' ERR;
 
 while read package ; do 
     echo Running ---- conda build $* $package;
-    conda build $* $package;
+    conda build --no-test $* $package;
     echo Result ----- $err
 done < build-order-nersc;
 
