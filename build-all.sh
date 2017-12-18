8a3242e0624bb5f019a49e4dc1b1d73ec0330126
Yu Feng <rainwoodman@gmail.com>
2017-12-18 14:08:17 -0800

    display the tail of log.

--- build-all.sh
+++ build-all.sh
@@ -4,8 +4,9 @@ trap 'exit 1' ERR;
 
 while read package ; do 
     echo Running ---- conda build $* $package;
-    conda build $* $package;
+    conda build $* $package 2>&1 > build-log-$package
     echo Result ----- $err
+    tail build-log-$package
 done < build-order;
 
 exit $err
