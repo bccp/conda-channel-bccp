6d77047c2407ab51e381d912a899b0e56cdbcd42
Yu Feng <rainwoodman@gmail.com>
2017-12-18 14:19:32 -0800

    use tmp filename for buildlog. (path errors)

--- build-all.sh
+++ build-all.sh
@@ -4,9 +4,10 @@ trap 'exit 1' ERR;
 
 while read package ; do 
     echo Running ---- conda build $* $package;
-    conda build $* $package 2>&1 > build-log-$package
+    buildlog=`mktemp`
+    conda build $* $package 2>&1 > $buildlog
     echo Result ----- $err
-    tail build-log-$package
+    tail $buildlog
 done < build-order;
 
 exit $err
