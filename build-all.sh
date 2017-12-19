4d14dd0507f1fb9865093f6d33937bdb8ed94208
Yu Feng <rainwoodman@gmail.com>
2017-12-18 23:21:08 -0800

    use cat instead of tail -f.
    tail -f seems to give stalled output still.

--- build-all.sh
+++ build-all.sh
@@ -4,7 +4,7 @@ trap 'exit 1' ERR;
 
 while read package ; do 
     echo Running ---- conda build $* $package;
-    conda build $* $package 2>&1 | tail -f
+    conda build $* $package 2>&1 | cat
     echo Result ----- $err
 done < build-order;
 
