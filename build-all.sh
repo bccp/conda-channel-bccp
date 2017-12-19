04eaea100f60df0034c618d51fbc7ec9e6ee456f
Yu Feng <rainwoodman@gmail.com>
2017-12-19 09:44:24 -0800

    print new line instead of the line.
    avoid potentially too long lines.

--- build-all.sh
+++ build-all.sh
@@ -5,10 +5,10 @@ trap 'exit 1' ERR;
 while read package ; do 
     echo Running ---- conda build $* $package;
     logfile=`mktemp XXXXX`
-    conda build $* $package 2>&1 | tee $logfile | awk "{printf(\".\")} NR % 40 == 0 {print} END {print}"
+    conda build $* $package 2>&1 | tee $logfile | awk "{printf(\".\")} NR % 40 == 0 {printf(\"\n\")} END {printf(\"\n\")}"
     echo Result ----- $err
     tail $logfile
-    rm -rf $logfile
+    rm -f $logfile
 done < build-order;
 
 exit $err
