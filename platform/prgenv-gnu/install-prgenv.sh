00cb7721f80ffb5c0ba4f7d310fb98c037f79b49
Your Name <you@example.com>
2017-12-25 14:29:09 -0800

    add include and libs to prgenv compilers.

--- platform/prgenv-gnu/install-prgenv.sh
+++ platform/prgenv-gnu/install-prgenv.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-install $RECIPE_DIR/mpicc $PREFIX/bin/
-install $RECIPE_DIR/mpicxx $PREFIX/bin/
-install $RECIPE_DIR/mpifort $PREFIX/bin/
-install $RECIPE_DIR/mpif77 $PREFIX/bin/
-install $RECIPE_DIR/mpif90 $PREFIX/bin/
+for fn in \
+    mpicc \
+    mpicxx \
+    mpifort \
+    mpif77 \
+    mpif90 \
+    mpi.conf \
+    ; do
+    sed -e \
+       "s;@PREFIX@;$PREFIX;g"  \
+       $RECIPE_DIR/${fn}.in >$fn
+    chmod +x $fn
+done
+
+for fn in \
+    mpicc \
+    mpicxx \
+    mpifort \
+    mpif77 \
+    mpif90 \
+    ; do
+    install $fn $PREFIX/bin/
+done
+
+install mpi.conf $PREFIX/etc/
