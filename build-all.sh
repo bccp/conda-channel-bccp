91dc19e38203165d540c9cbadbcb69d63054127c
Yu Feng <rainwoodman@gmail.com>
2017-12-24 23:52:16 -0800

    strip prefix from pkglist.

--- build-all.sh
+++ build-all.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
-if [ "x$1" == x ]; then
-    echo  "Usage: bash build-all.sh packagelistfile [conda-build args ...]"
+if [ "x$2" == x ]; then
+    echo  "Usage: bash build-all.sh packagelistdir packagelistfile [conda-build args ...]"
     exit 1
 fi
 
-pkglist=$1
+pkgroot=$1
+pkglist=$2
+shift
 shift
 
 err=0
@@ -17,9 +19,9 @@ while read package ; do
         echo bypassing comment $package
         continue
     fi
-    echo Running ---- conda build $* $package;
+    echo Running ---- conda build $* $pkgroot/$package;
     logfile=`mktemp XXXXX`
-    conda build $* $package 2>&1 | tee $logfile | awk "{printf(\".\")} NR % 40 == 0 {printf(\"\n\")} END {printf(\"\n\")}"
+    conda build $* $pkgroot/$package 2>&1 | tee $logfile | awk "{printf(\".\")} NR % 40 == 0 {printf(\"\n\")} END {printf(\"\n\")}"
     echo Result ----- $err
     tail $logfile
     rm -f $logfile
