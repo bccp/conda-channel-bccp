93b09c028fd60e6c8d81e8e2de01152bd289fdfb
Yu Feng <rainwoodman@gmail.com>
2018-01-11 15:49:21 -0800

    force using /bin/bash
    some systems do not have /usr/bin/bash
    
    Also trigger a rebuild because our current ones on the channel
    seem to have extra -fdebug-xxxx flags that breaks debugging badly.

--- platform/mpich2/build.sh
+++ platform/mpich2/build.sh
@@ -3,7 +3,7 @@
 # don't know if this is useful.
 export FCFLAGS="$FFLAGS"
 
-./configure --enable-shared --prefix=$PREFIX "F77=$FC" "FCFLAGS=$FFLAGS"
+./configure --enable-shared --prefix=$PREFIX "F77=$FC" "FCFLAGS=$FFLAGS" "BASH_SHELL=/bin/bash"
 make
 make install
 
