527fb2fb5eaa15402658e289caf31d027e1d3f8e
Yu Feng <rainwoodman@gmail.com>
2018-03-29 09:45:19 -0700

    update mpich2 script to use a working set of env vars.
    Also merge build and host -- otherwise the wrapper will be
    hard wired to use binaries from the build env, and fail the
    path replacement.

--- platform/mpich2/build.sh
+++ platform/mpich2/build.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# don't know if this is useful.
-export FCFLAGS="$FFLAGS"
+# pretend we are not in conda build
+# to avoid special 'helpful' debug flags
+# that breaks the mpicc/mpif90 wrappers.
 
-./configure --enable-shared --prefix=$PREFIX "F77=$FC" "FCFLAGS=$FFLAGS" "BASH_SHELL=/bin/bash"
+export FFLAGS="-fopenmp -march=nocona -mtune=haswell -ftree-vectorize -fPIC -fstack-protector-strong -fno-plt -O2 -pipe"
+export CFLAGS="-march=nocona -mtune=haswell -ftree-vectorize -fPIC -fstack-protector-strong -fno-plt -O2 -pipe"
+export CXXFLAGS="-fvisibility-inlines-hidden -std=c++17 -fmessage-length=0 -march=nocona -mtune=haswell -ftree-vectorize -fPIC -fstack-protector-strong -fno-plt -O2 -pipe"
+
+# don't know if this is useful.
+export FCFLAGS="${FFLAGS}"
+
+./configure --enable-shared --prefix=$PREFIX "F77=$FC" "BASH_SHELL=/bin/bash"
 make
 make install
 
 rm -rf $PREFIX/share
 rm -rf $PREFIX/sbin
-if grep '/usr/local/src/conda-prefix' $PREFIX/bin/mpi*; then
-    exit 1
-fi
