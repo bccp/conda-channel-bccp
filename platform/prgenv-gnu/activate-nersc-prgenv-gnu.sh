40bb81e491d19cb58704ed5d50b092ce24428207
Your Name <you@example.com>
2018-06-15 18:18:08 -0700

    Set LDSHARED and other variables.

--- platform/prgenv-gnu/activate-nersc-prgenv-gnu.sh
+++ platform/prgenv-gnu/activate-nersc-prgenv-gnu.sh
@@ -16,9 +16,22 @@ export CC=cc
 export CXX=CC
 export FC=ftn
 export F77=ftn
+
 # set a reasonable number for number of threads,
 # to avoid 'pthreads_create: Resource temporarily unavailable' error.
 export OMP_NUM_THREADS=2
 
 export _OLD_CONDA_PYTHON_SYSCONFIGDATA_NAME=${_CONDA_PYTHON_SYSCONFIGDATA_NAME}
 export _CONDA_PYTHON_SYSCONFIGDATA_NAME=_sysconfigdata_x86_64_conda_nersc_prgenv_gnu
+
+# due to https://github.com/ContinuumIO/anaconda-issues/issues/9621
+# the above line doesn't override LDSHARED in PYTHON,
+# causing linking failures
+# therefore, we set LDSHARED explicitly here
+export LDSHARED="cc -pthread -shared"
+export MPICH_MAX_THREAD_SAFETY=multiple
+
+# recommended by Stephen Bailey for better multiprocessing support.
+export KMP_AFFINITY=disabled
+export MPICH_GNI_FORK_MODE=FULLCOPY
+
