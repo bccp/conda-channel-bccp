a87009e3283e25fc741c9ea31851fb29cb707ded
Yu Feng <rainwoodman@gmail.com>
2018-09-10 17:51:26 -0700

    reenable building of mpich 3.x

--- platform/mpich/build.sh
+++ platform/mpich/build.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-if [[ $OSTYPE == darwin* ]]; then
-    # this may need to be done on linux too,
-    # see https://github.com/ContinuumIO/anaconda-issues/issues/739
-    export LDFLAGS="$LDFLAGS -Wl,-rpath,${CONDA_PREFIX}/lib"
-fi
+# we type these by hand instead of using the values 
+# provided by conda-build:
+
+# 1. to avoid special 'helpful' debug flags
+# that breaks the mpicc/mpif90 wrappers.
+
+# 2. also avoids many -f flags that causes a weird
+# cmake `;list` conversion bug (either in cmake or the scripts)
+# https://github.com/pytorch/pytorch/issues/8466
+
+export FFLAGS="-fopenmp -march=nocona -mtune=haswell -fPIC -fstack-protector-strong -O2 -pipe"
+export CFLAGS="-march=nocona -mtune=haswell -fPIC -fstack-protector-strong -O2 -pipe"
+export CXXFLAGS="-std=c++17 -march=nocona -mtune=haswell -fPIC -fstack-protector-strong -O2 -pipe"
 
 # don't know if this is useful.
 export FCFLAGS="$FFLAGS"
+export FC="$F90"
+
+# don't know if this is useful.
+export -n F90FLAGS
+export -n F90
 
 ./configure --prefix=$PREFIX \
             --disable-dependency-tracking \
@@ -17,3 +30,6 @@ export FCFLAGS="$FFLAGS"
 
 make -j$CPU_COUNT
 make install
+
+rm -rf $PREFIX/share
+rm -rf $PREFIX/sbin
