b47ac1e814d3ab93c94195b1782228d0fd1a9ecc
He Jia <he.jia.phy@gmail.com>
2019-02-14 15:17:22 -0800

    Update build.sh

--- platform/pytorch-cpu/build.sh
+++ platform/pytorch-cpu/build.sh
@@ -3,5 +3,10 @@ export NO_CUDA=1
 export CMAKE_PREFIX_PATH=$PREFIX
 
 export VERBOSE=1
-patch -p1 -d third_party/gloo < ${RECIPE_DIR}/gloo-SPEED_UNKNOWN.patch
 $PYTHON setup.py install
+
+echo -e "If you want to build libtorch, please do\n"
+echo "cd <pytorch_root>"
+echo "mkdir build_libtorch && cd build_libtorch"
+echo "BUILD_TORCH=ON ONNX_NAMESPACE=onnx_torch bash ../tools/build_pytorch_libs.sh --use-nnpack caffe2"
+echo -e "\nThen the output will be produced at torch/lib/tmp_install"
