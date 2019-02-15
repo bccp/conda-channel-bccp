02e5292eb7def1d5954acb08f2077cd4504f7b93
He Jia <he.jia.phy@gmail.com>
2019-02-15 10:48:34 -0800

    Build libtorch

--- platform/pytorch-cpu/build.sh
+++ platform/pytorch-cpu/build.sh
@@ -5,8 +5,14 @@ export CMAKE_PREFIX_PATH=$PREFIX
 export VERBOSE=1
 $PYTHON setup.py install
 
-echo -e "If you want to build libtorch, please do\n"
-echo "cd <pytorch_root>"
-echo "mkdir build_libtorch && cd build_libtorch"
-echo "BUILD_TORCH=ON ONNX_NAMESPACE=onnx_torch bash ../tools/build_pytorch_libs.sh --use-nnpack caffe2"
-echo -e "\nThen the output will be produced at torch/lib/tmp_install"
+{
+  {
+  mkdir build_libtorch
+  cd build_libtorch
+  BUILD_TORCH=ON ONNX_NAMESPACE=onnx_torch bash ../tools/build_pytorch_libs.sh --use-nnpack caffe2
+  } && {
+  echo -e "\nSuccessfully built libtorch at `dirname $PWD`/torch/lib/tmp_install\n"
+  }
+} || {
+  echo -e "\nError: failed to build libtorch"
+}
