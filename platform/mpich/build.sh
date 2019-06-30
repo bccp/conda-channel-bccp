780580c6514d1718141343174a17acbeb10e9991
Yu Feng <rainwoodman@gmail.com>
2019-06-30 14:14:09 -0700

    Follow new conda-forge mpich.
    See if it works

--- platform/mpich/build.sh
+++ platform/mpich/build.sh
@@ -1,40 +1,51 @@
 #!/bin/bash
 
-# we type these by hand instead of using the values 
-# provided by conda-build:
+# configure balks if F90 is defined
+# with a fatal deprecation message pointing to FC
+unset F90 F77
 
-# 1. to avoid special 'helpful' debug flags
-# that breaks the mpicc/mpif90 wrappers.
+# remove --as-needed, which causes problems for downstream builds,
+# seen in failures in petsc, slepc, and hdf5 at least
+export LDFLAGS="${LDFLAGS/-Wl,--as-needed/}"
 
-# 2. also avoids many -f flags that causes a weird
-# cmake `;list` conversion bug (either in cmake or the scripts)
-# https://github.com/pytorch/pytorch/issues/8466
+# avoid absolute-paths in compilers
+export CC=$(basename "$CC")
+export CXX=$(basename "$CXX")
+export FC=$(basename "$FC")
 
-if [[ $OSTYPE == darwin* ]]; then
-  export FFLAGS="-march=nocona -mtune=core2 -ftree-vectorize -fPIC -fstack-protector -O2 -pipe"
-else
-  export FFLAGS="-fopenmp -march=nocona -mtune=haswell -fPIC -fstack-protector-strong -Og -g -pipe"
-  export CFLAGS="-march=nocona -mtune=haswell -fPIC -fstack-protector-strong -Og -g -pipe"
-  export CXXFLAGS="-std=c++17 -march=nocona -mtune=haswell -fPIC -fstack-protector-strong -Og -g -pipe"
-fi
-
-# don't know if this is useful.
+# from anaconda recipe, not sure if it matters
 export FCFLAGS="$FFLAGS"
-export FORTRANFLAGS="$FFLAGS"
-export FC="$F90"
 
-# don't know if this is useful.
-export -n F90FLAGS
-export -n F90
+# avoid recording flags in compilers
+# See Compiler Flags section of MPICH readme
+export MPICHLIB_CFLAGS=$CFLAGS
+unset CFLAGS
+export MPICHLIB_CPPFLAGS=$CPPFLAGS
+unset CPPFLAGS
+export MPICHLIB_CXXFLAGS=$CXXFLAGS
+unset CXXFLAGS
+export MPICHLIB_LDFLAGS=$LDFLAGS
+unset LDFLAGS
+export MPICHLIB_FFLAGS=$FFLAGS
+unset FFLAGS
+export MPICHLIB_FCFLAGS=$FCFLAGS
+unset FCFLAGS
+
+# set some specific flags that we *do* want recorded in the compilers
+# only the bare minimum of prefix-awareness here
+export CFLAGS="-I$PREFIX/include"
+export CPPFLAGS="-I$PREFIX/include"
+export CXXFLAGS="-I$PREFIX/include"
+export FFLAGS="-I$PREFIX/include"
+export LDFLAGS="-L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
+
+export LIBRARY_PATH="$PREFIX/lib"
 
 ./configure --prefix=$PREFIX \
             --disable-dependency-tracking \
+            --disable-wrapper-rpath \
             --enable-cxx \
-            --enable-f77 \
-            --enable-fc
+            --enable-fortran
 
-make -j$CPU_COUNT
+make -j"${CPU_COUNT:-1}"
 make install
-
-rm -rf $PREFIX/share
-rm -rf $PREFIX/sbin
