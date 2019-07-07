7c4238fece05e403bcd1c9224252b9dc0e8360c3
Yu Feng <rainwoodman@gmail.com>
2019-07-07 00:21:35 -0700

    osx fixes.

---
+++ osxinit.sh
@@ -0,0 +1,9 @@
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall > ~/uninstall_homebrew
+chmod +x ~/uninstall_homebrew
+~/uninstall_homebrew -fq
+
+cat > conda_build_config.yaml <<END
+CONDA_BUILD_SYSROOT:
+- $(xcode-select -p)/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk # [osx]
+END
+cat conda_build_config.yaml
