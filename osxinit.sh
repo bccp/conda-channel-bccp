dcbac91445584525e546535497d1533fdfb6a8ee
Yu Feng <rainwoodman@gmail.com>
2019-07-07 00:08:39 -0700

    run osxinit in before

--- osxinit.sh
+++ osxinit.sh
@@ -1,7 +1,9 @@
-if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall > ~/uninstall_homebrew
+chmod +x ~/uninstall_homebrew
+~/uninstall_homebrew -fq
+
 cat > conda_build_config.yml <<END
 CONDA_BUILD_SYSROOT:
 - $(xcode-select -p)/Platforms/MacOSX.platform/Developer/SDKs/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk # [osx]
 END
 cat conda_build_config.yml
-fi
